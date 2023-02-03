f66bf63a9d3e50bda5a0e3985bf4b32012f016c6
mysteriousmy <jackmysterious@outlook.com>
2023-02-03 11:25:25 +0800

    update

--- CorePress.ui/src/App.tsx
+++ CorePress.ui/src/App.tsx
@@ -2,11 +2,14 @@ import type { Component } from 'solid-js';
 
 import logo from './logo.svg';
 import styles from './App.module.css';
+import { Button } from '@suid/material';
 
 const App: Component = () => {
   return (
     <div class={styles.App}>
       <header class={styles.header}>
+        
+        <Button variant="contained">Hello wrold</Button>
         <img src={logo} class={styles.logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
